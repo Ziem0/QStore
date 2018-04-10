a644e340539fb57ac36874f6b413595d301fb945
Ziem0 <andrzejewski.ziemo@gmail.com>
2018-04-10 20:19:43 +0200

    merge2

--- src/Checker.java
+++ src/Checker.java
@@ -2,14 +2,6 @@
 public class Checker {
 
     public static void main(String[] args) {
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
-          
-=======
-          String a;
->>>>>>> Stashed changes
-=======
-          String a;
->>>>>>> Stashed changes
+
     }
 }
