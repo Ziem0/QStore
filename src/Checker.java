1c6b7f472fc50b583df8f4fd54877a153479aaae
Ziem0 <andrzejewski.ziemo@gmail.com>
2018-04-10 20:16:47 +0200

    merge conflict

--- src/Checker.java
+++ src/Checker.java
@@ -3,7 +3,11 @@ public class Checker {
 
     public static void main(String[] args) {
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
           
+=======
+          String a;
+>>>>>>> Stashed changes
 =======
           String a;
 >>>>>>> Stashed changes
